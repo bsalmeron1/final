0ecd738b39f87f7a04824a389dbfe7c9025259cb
bsalmeron1 <bsalmeron1426@gmail.com>
2026-02-10 21:09:15 +0000

    DAY ONE

---
+++ catcafe.py
@@ -0,0 +1,23 @@
+
+# Jacqueline Sandoval. Breanna Tepetongo, Adrian Marqetequesias
+
+
+name = input( "What's your name?: " ) #f string so we have to put it in parenthesis but before quotes when referencing it
+def nowant(answer):
+    if answer == "yes": 
+        print(f" Awesome {name}-chan!")
+    elif answer == "no":
+        print(f"No... {name}-chan, dom't starve yourself, but ok!: ")  #f infront in order to refer to the name in the output of the 
+print(f"Welcome {name}-Chan, welcome to the Cat Cafe! ^-^") #were gunna make two of these one being  drink one and patry to refer to their seperate lists easier
+
+answer= input("Do you want a drink?:  ")
+
+nowant(answer)
+
+
+
+ 
+drink = ['#0 - boba', '#1 - breanna pee', '#2 - jackie direhea']
+
+pastries=['fish tentacle', 'pompri placenta', 'konguru pickled ramble']
+if
